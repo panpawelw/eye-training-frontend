ab45182c1f33916295bc0c5ac814f96e9ae0ab9b
Paweł Wojtuń <panpawelw@pm.me>
2021-12-17 23:35:58 +0100

    Set up routes, add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { GameGaborComponent } from './game-gabor/game-gabor.component';
+import { GameMatch3Component } from './game-match3/game-match3.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'gabor', component: GameGaborComponent },
+  { path: 'match3', component: GameMatch3Component },
+  { path: '',   redirectTo: '/gabor', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
