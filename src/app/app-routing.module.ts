2c6a2ed0b372b484a2bffaf315b306f2a4906a8b
Paweł Wojtuń <panpawelw@pm.me>
2021-12-21 20:09:55 +0100

    Center menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { GameGaborComponent } from './game-gabor/game-gabor.component';
 import { GameMatch3Component } from './game-match3/game-match3.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   { path: 'gabor', component: GameGaborComponent },
   { path: 'match3', component: GameMatch3Component },
-  { path: '',   redirectTo: '/gabor', pathMatch: 'full' }
+  { path: '',   redirectTo: '/gabor', pathMatch: 'full' },
+  { path: '', component: MenuComponent, outlet: "menu"}
 ];
 
 @NgModule({
