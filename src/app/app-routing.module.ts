3789eefa7e7068e11389e65c09658bfd72997e1a
Paweł Wojtuń <panpawelw@pm.me>
2021-12-29 23:57:07 +0100

    Add routing to URL not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { GameMatch3Component } from './game-match3/game-match3.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'gabor', component: GameGaborComponent },
   { path: 'match3', component: GameMatch3Component },
   { path: '',   redirectTo: '/gabor', pathMatch: 'full' },
-  { path: '', component: MenuComponent, outlet: "menu"}
+  { path: '', component: MenuComponent, outlet: "menu" },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
